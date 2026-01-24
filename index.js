6e53de18ac1c72532198d9463026622b0bec8076
christian <nwolle14@gmail.com>
2026-01-24 05:23:01 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//   const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+//   document.querySelectorAll("nav a").forEach(link => {
+//     if (link.getAttribute("href") === currentPage) {
+//       link.classList.add("active");
+//     }
+//   });
+
